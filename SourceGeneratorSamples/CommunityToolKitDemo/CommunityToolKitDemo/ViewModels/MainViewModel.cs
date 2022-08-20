f3a1be2b6b87d41637f2285d16beb509d9426733
Jigar Tailor <jigartailor1984@live.com>
2022-08-20 14:56:10 +0530

    Added Sample code of use of communitytoolkit  source generator in DotNet Core and .NetFramework Projects

---
+++ SourceGeneratorSamples/CommunityToolKitDemo/CommunityToolKitDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityToolKitDemo.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string _text;
+
+        [RelayCommand]
+        void Add()
+        { 
+        
+        }
+
+        public MainViewModel()
+        {
+            Text = string.Empty;
+        }
+    }
+}
