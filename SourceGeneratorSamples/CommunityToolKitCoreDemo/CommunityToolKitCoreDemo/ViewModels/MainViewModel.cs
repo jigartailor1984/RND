2e3d9eb96c8fdcb64d7decbc614765357003287b
Jigar Tailor <jigartailor1984@live.com>
2023-01-22 13:41:34 +0530

    change for hosting package and counter on button

--- SourceGeneratorSamples/CommunityToolKitCoreDemo/CommunityToolKitCoreDemo/ViewModels/MainViewModel.cs
+++ SourceGeneratorSamples/CommunityToolKitCoreDemo/CommunityToolKitCoreDemo/ViewModels/MainViewModel.cs
@@ -2,24 +2,29 @@
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CommunityToolKitCoreDemo.ViewModels
 {
-    public partial class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableValidator
     {
+        int _counter = 0;
         [ObservableProperty]
-        string _text;
+        string _text = string.Empty;
 
         [RelayCommand]
         void Add()
-        { 
+        {
+
+            Text = $"Add clicked {++_counter} Times!!";
         }
         public MainViewModel()
         {
-            
+            Text = "Click Me!";
         }
     }
 }
