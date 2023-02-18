d336b96a9132a5c549a83da685fd488ec43e6ffc
Jigar Tailor <jigartailor1984@live.com>
2023-02-18 13:05:48 +0530

    Changes to user Validation Shource generator

---
+++ SourceGeneratorSamples/CommunityToolKitCoreDemo/CommunityToolKitCoreDemo/ViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CommunityToolKitCoreDemo.ViewModels
+{
+    public partial class UserDetailsViewModel : ObservableValidator
+    {
+        [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        private string? firstName;
+
+        [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        private string? lastName;
+
+        [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [Required]
+        [EmailAddress]
+        private string? email;
+
+        [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [Required]
+        [Phone()]
+        private string? phoneNumber;
+
+        [RelayCommand]
+        void Submit()
+        { 
+            ValidateAllProperties();
+            if(this.HasErrors)
+            {
+                MessageBox.Show("Validation failed");
+            }
+            else
+            {
+                MessageBox.Show("Submition successfull.");
+            }
+        }
+    }
+}
